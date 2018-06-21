a65dc51b034109b907fdf108c712ccd7ed77f722
James Forcier <james.forcier@coreos.com>
2018-06-21 14:13:17 -0700

    ergodox_ez/keymaps/csssuf: add PgUp/PgDn in L1

--- keyboards/ergodox_ez/keymaps/csssuf/keymap.c
+++ keyboards/ergodox_ez/keymaps/csssuf/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |  !   |  @   |  #   |  $   |  %   |      |           |      |      |  =   |  -   |  _   |  +   |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |  ^   |  &   |  *   |  :   |  ~   |------|           |------|  (   |  )   |  [   |  ]   |  :   |   '    |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| PgDn |           | PgUp |------+------+------+------+------+--------|
  * |        |  \   |  |   |  '   |  "   |  ?   |      |           |      |  {   |  }   |  <   |  >   |  ?   |  Calc  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |      |      |      |      |      |                                       |      |      |      |      |      |
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TILD,
        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_TRNS,
        KC_TRNS, KC_CIRC, KC_AMPR, KC_ASTR, KC_COLN, KC_TILD,
-       KC_TRNS, KC_BSLS, KC_PIPE, KC_QUOT, KC_DQUO, KC_QUES, KC_TRNS,
+       KC_TRNS, KC_BSLS, KC_PIPE, KC_QUOT, KC_DQUO, KC_QUES, KC_PGDN,
        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                        KC_TRNS, KC_TRNS,
                                                 KC_TRNS,
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
        KC_TRNS, KC_TRNS, KC_EQL,  KC_MINS, KC_UNDS, KC_PLUS, KC_BSLS,
                 KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC, KC_COLN, KC_QUOT,
-       KC_TRNS, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   KC_QUES, KC_CALC,
+       KC_PGUP, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   KC_QUES, KC_CALC,
                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
